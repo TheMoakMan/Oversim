d7f75ad81c5c047c51888537023d5129fcd55458
evanDesktop <mr.moakman2014@gmail.com>
2018-11-15 00:18:57 -0600

    Added Webpages

--- website.py
+++ website.py
@@ -6,8 +6,28 @@ def hello_world():
     return "Welcome to Oversim Fantasy Overwatch Simulator"
 
 @app.route('/login')
-def login():
+def login_page():
     return "Please enter a username and password:"
+
+@app.route('/teams')
+def teams_page():
+    return "Teams"
+
+@app.route('/play')
+def play_page():
+    return "Simulation Page"
+
+@app.route('/account')
+def account_info():
+    return "Account Info"
+
+@app.route('/players')
+def players_page():
+    return "Players Page"
+
+@app.route('/heroes')
+def heroes_page():
+    return "All heroes up to patch 0.0.0"
 
 if __name__ == '__main__':
     app.run()
