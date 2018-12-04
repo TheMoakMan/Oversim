54967ae17fcbac4b04f77af4f6c5688f66919e97
evanDesktop <mr.moakman2014@gmail.com>
2018-12-04 09:14:08 -0600

    Added proper project folder

--- FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/views.py
@@ -9,23 +9,11 @@ from FlaskWebProject1 import app
 import pypyodbc
 
 #Create Connection Object for SQL Server Connection
-connection = pypyodbc.connect("DRIVER={SQL SERVER}; Server=DESKTOP-LVNF7SR\Evan; Trusted_Connection=True; DATABASE=Oversim") 
-cursor = connection.cursor()
+connection = pypyodbc.connect("DRIVER={SQL SERVER}; Server=DESKTOP-LVNF7SR; Trusted_Connection=True; DATABASE=Oversim") 
 
-hero_selection = "'Ana'"
-cursor.execute("select * from Hero where name = \'Ana\';")
+#Get current user from login table at somepoint from website
+curr_user = "Doug"
 
-def get_username():
-    
-
-results = cursor.fetchall()
-
-table = "|"
-for tuple in results:
-    for attribute in tuple:
-        table += str(attribute) + "|"
-
-print(table)
 
 @app.route('/')
 @app.route('/home.html')
@@ -33,28 +21,43 @@ def home():
     """Renders the home page."""
     return render_template(
            "home.html",
-           username = "BIGGOOSE"
+           username = curr_user
         )
 
 @app.route('/teams.html')
 def team():
+    #Select all pro teams
+    cursor = connection.cursor()
+    cursor.execute("select * from Team where is_pro = 1;")
+    results = cursor.fetchall()
+    cursor.close()
+
+    #Get players on those teams
+    cursor = connection.cursor()
+    cursor.execute("select ID, Player, Role, PlayerPhoto from Teams_All_Players where PRO = 1;")
+    teams_Players = cursor.fetchall()
+    cursor.close()
+
     """Renders the team page."""
     return render_template(
           "teams.html",
-          username = "10kGeese",
-          team_0_city = "Dallas",
-          team_0_mascot = "Fuel",
-          team_1_city = "London",
-          team_1_mascot = "Spitfire",
-          info = "Pro Team"
+          username = curr_user,
+          teamTable = results,
+          teamPlayers = teams_Players
         )
 
 @app.route('/heroes.html')
 def heroes():
+    cursor = connection.cursor()
+    cursor.execute("select * from Hero")
+    h_results = cursor.fetchall()
+    cursor.close()
+
     """Renders the heroes page."""
     return render_template(
           "heroes.html",
-          username = "10kGeese"
+          username = curr_user,
+          heroTable = h_results,
           )
 
 @app.route('/play.html')
@@ -62,25 +65,47 @@ def play_main_page():
     """Renders the play page."""
     return render_template(
           "play.html",
-          username = "10kGeese"
+          username = curr_user
           )
 
 
 @app.route('/players.html')
 def players():
-    """Renders the players page."""
+   #Select all players
+    cursor = connection.cursor()
+    cursor.execute("select * from All_Players_ON_Teams;")
+    results = cursor.fetchall()
+    cursor.close()
+
+    """Renders the myteams page."""
     return render_template(
           "players.html",
-          username = "10kGeese"
-          )
+          username = curr_user,
+          playerTable = results
+         )
 
 
 @app.route('/myteam.html')
 def myteams():
+    #Select all users teams
+    cursor = connection.cursor()
+    cursor.execute("select * from Team where is_pro = 0 AND OWNER = \'" + curr_user + "\';")
+    results = cursor.fetchall()
+    cursor.close()
+
+    #Get players on those teams
+    cursor = connection.cursor()
+    cursor.execute("""select ID, Player, Role, PlayerPhoto 
+                      from Teams_All_Players where PRO = 0 AND OWNER = \'""" + curr_user + "\';")
+    teams_Players = cursor.fetchall()
+    cursor.close()
+
     """Renders the myteams page."""
     return render_template(
           "myteam.html",
-          username = "10kGeese"
+          username = curr_user,
+          teamTable = results,
+          teamPlayers = teams_Players
          )
 
 @app.route('/account.html')
@@ -88,7 +113,7 @@ def account():
     """Renders the account page."""
     return render_template(
           "account.html",
-          username = "10kGeese"
+          username = curr_user
           )
 
 
