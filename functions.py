a3ef2c3fc3967bc1fefa9138d994ef67c36f03b1
evanDesktop <mr.moakman2014@gmail.com>
2018-12-05 23:38:36 -0600

    ;lasjdfaslkdjf

--- functions.py
+++
@@ -1,6 +0,0 @@
-def site_title():
-    return "Oversim, Overwatch Fantasy Simulator"
-
-def site_desc():
-    return "Welcome to Oversim!, Premeire site for simulating Overwatch Dream Team Matchups"
-
